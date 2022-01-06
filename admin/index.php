80829bb5ae4821e3e9edb0bda9278e29848e8fa4
soumri nadhem <blendplay2@gmail.com>
2022-01-06 16:46:01 +0100

    admin panel design

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Food Order</title>
+
+        <link rel="stylesheet" href="">
+    </head>
+   <body>
+       <!--Menu Section Starts -->
+       <div class="menu">
+        Menu Goes Here
+       </div>
+       <!--Menu Section Ends -->
+
+       <!--Main Content Section Starts -->
+       <div class="main-content">
+        Menu Content Goes Here
+       </div>
+       <!--Main Content Section Ends  -->
+
+
+        <!--Footer Section Starts -->
+        <div class="footer">
+        footer Goes Here
+       </div>
+        <!--Footer Section Ends -->
+   </body>
+</html>
